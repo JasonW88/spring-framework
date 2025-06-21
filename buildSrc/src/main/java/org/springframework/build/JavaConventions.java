a7b0efc4092c1ec7fcd959eaa243254467f4d581
Alex Cook <a5cook@uwaterloo.ca>
2025-06-21 11:58:47 -0400

    build: remove processing warnings.

--- buildSrc/src/main/java/org/springframework/build/JavaConventions.java
+++ buildSrc/src/main/java/org/springframework/build/JavaConventions.java
@@ -57,7 +57,7 @@ public class JavaConventions {
 		List<String> commonCompilerArgs = List.of(
 				"-Xlint:serial", "-Xlint:cast", "-Xlint:classfile", "-Xlint:dep-ann",
 				"-Xlint:divzero", "-Xlint:empty", "-Xlint:finally", "-Xlint:overrides",
-				"-Xlint:path", "-Xlint:processing", "-Xlint:static", "-Xlint:try", "-Xlint:-options",
+				"-Xlint:path", "-Xlint:-processing", "-Xlint:static", "-Xlint:try", "-Xlint:-options",
 				"-parameters"
 		);
 		COMPILER_ARGS = new ArrayList<>();
